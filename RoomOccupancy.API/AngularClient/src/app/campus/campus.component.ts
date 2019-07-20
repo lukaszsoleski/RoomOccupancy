d76fb133e19e4daef07743a5a9429edeb7682980
Łukasz Soleski <luk.soleski@gmail.com>
2019-07-20 19:10:23 +0200

    Fix styles for mapify plugin

--- RoomOccupancy.API/AngularClient/src/app/campus/campus.component.ts
+++ RoomOccupancy.API/AngularClient/src/app/campus/campus.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare const $:any;
 @Component({
   selector: 'app-campus',
   templateUrl: './campus.component.html',
@@ -10,6 +10,27 @@ export class CampusComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  
+    this.mapSetup();
+    
+  }
+  
+  private mapSetup() : void{
+    $(function(){
+	
+      $("img[usemap]").mapify({
+      hoverClass: "custom-hover",
+      popOver: {
+        content: function(zone){ 
+            return "<strong>"+zone.attr("data-title")+"</strong>"+zone.attr("data-translate");
+        },
+        delay: 0.7,
+        margin: "5px",
+        height: "130px",
+        width: "250px"
+      }
+      });
+  });
   }
 
 }
