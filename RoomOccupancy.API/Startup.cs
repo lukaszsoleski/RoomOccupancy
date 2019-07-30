5819d7d3e44caccdb4251d7f0d6e30256ba3aad7
Łukasz Soleski <luk.soleski@gmail.com>
2019-07-30 08:06:04 +0200

    Configuration

--- RoomOccupancy.API/Startup.cs
+++ RoomOccupancy.API/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RoomOccupancy.Application.Interfaces;
+using RoomOccupancy.Persistence;
 
 namespace RoomOccupancy.API
 {
@@ -25,6 +28,11 @@ namespace RoomOccupancy.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configure DB Context. Add EbookShop context to dependency injection container and set database provider and also connection string. 
+            services.AddDbContext<IReservationDbContext,ReservationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("RoomOccupancyDatabase")));
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
