8648b2bef4cbc17e036283f0594144690c4295c4
Łukasz Soleski <luk.soleski@gmail.com>
2019-08-05 01:16:01 +0200

    Add CreateRoomCommand tests to check if the object has been added to the context collection.

--- RoomOccupancy.Application.Tests/Campus/Rooms/CreateRoomCommandTests.cs
+++ RoomOccupancy.Application.Tests/Campus/Rooms/CreateRoomCommandTests.cs
@@ -7,9 +7,11 @@ using Moq.AutoMock;
 using RoomOccupancy.Application.Campus.Rooms.Commands.CreateRoom;
 using RoomOccupancy.Application.Exceptions;
 using RoomOccupancy.Application.Interfaces;
+using RoomOccupancy.Application.Notifications;
 using RoomOccupancy.Domain.Entities.Campus;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -25,30 +27,70 @@ namespace RoomOccupancy.Application.Tests.Campus.Rooms
             //arrange
             var mocker = new AutoMocker();
             var buildingId = 1; 
-            var handler = mocker.CreateInstance<CreateRoomCommand.Handler>();
             // mapper moq
             mocker.GetMock<IMapper>().Setup(x => x.Map<Room>(It.IsAny<CreateRoomCommand>()))
                 .Returns(new Room() { BuildingId = buildingId });
 
             mocker.GetMock<IReservationDbContext>().Setup(x => x.Buildings.FindAsync(It.IsAny<int>()))
-                .Returns(Task.FromResult((Building)null)); 
+                .Returns(Task.FromResult((Building)null));
 
             //act
+            var handler = mocker.CreateInstance<CreateRoomCommand.Handler>();
+
             Func<Task> func = async () => await handler.Handle(new CreateRoomCommand(), CancellationToken.None);
             //assert 
             await func.Should().ThrowExactlyAsync<NotFoundException>()
                 .WithMessage($@"Entity ""{typeof(Building).Name}"" ({buildingId}) was not found."); 
         }
 
-        //[Fact]
-        //public async Task Handler_RoomCreated_SendsNotification()
-        //{
+        [Fact]
+        public async Task Handler_RoomCreated_SendsNotification()
+        {
+            //arrange
+            var hasBeenCalled = false;
+            var mocker = new AutoMocker();
+            var buildingId = 1;
 
-        //}
-        //[Fact]
-        //public async Task Handler_ValidRequest_RoomIsCreated()
-        //{
+            mocker.Use(MapperProfileBuilder.Mapper); 
+            mocker.GetMock<IMediator>().Setup(x => x.Publish(It.IsNotNull<RoomCreated>(),It.IsAny<CancellationToken>()))
+                .Callback( () => hasBeenCalled = true);
 
-        //}
+            var context = InMemoryContext.Create();
+            context.Buildings.Add(new Building() { Id = 1 });
+            context.SaveChanges();
+            mocker.Use<IReservationDbContext>(context);
+            //act
+            var handler = mocker.CreateInstance<CreateRoomCommand.Handler>();
+
+            await handler.Handle(new CreateRoomCommand() { BuildingId = buildingId }, CancellationToken.None);
+
+            //assert 
+            hasBeenCalled.Should().BeTrue();
+
+            context.Dispose();
+        }
+        [Fact]
+        public async Task Handler_ValidRequest_RoomIsCreated()
+        {
+            //arrange
+            var mocker = new AutoMocker();
+            var buildingId = 1;
+
+            mocker.Use(MapperProfileBuilder.Mapper);
+
+            var context = InMemoryContext.Create();
+            context.Buildings.Add(new Building() { Id = 1 });
+            context.SaveChanges();
+            mocker.Use<IReservationDbContext>(context);
+            //act
+            var handler = mocker.CreateInstance<CreateRoomCommand.Handler>();
+
+            await handler.Handle(new CreateRoomCommand() { BuildingId = buildingId }, CancellationToken.None);
+
+            //assert 
+            context.Rooms.Count().Should().Be(1);
+
+            context.Dispose();
+        }
     }
 }
--- RoomOccupancy.Application.Tests/InMemoryContext.cs
+++ RoomOccupancy.Application.Tests/InMemoryContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using RoomOccupancy.Persistence;
 using System;
 using System.Collections.Generic;
@@ -8,11 +9,29 @@ namespace RoomOccupancy.Application.Tests
 {
     public static class InMemoryContext
     {
-        public static ReservationDbContext Inst =>
-            new ReservationDbContext(
-                new DbContextOptionsBuilder<ReservationDbContext>()
-                .UseInMemoryDatabase("TestDb")
-                .Options
-            ); 
+
+        public static ReservationDbContext Create()
+        {
+            var options = CreateNewContextOptions();
+
+            var context = new ReservationDbContext(options);
+
+            return context; 
+        }
+        private static DbContextOptions<ReservationDbContext> CreateNewContextOptions()
+        {
+            // Create a fresh service provider, and therefore a fresh 
+            // InMemory database instance.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance telling the context to use an
+            // InMemory database and the new service provider.
+            var builder = new DbContextOptionsBuilder<ReservationDbContext>();
+            builder.UseInMemoryDatabase("Test")
+                   .UseInternalServiceProvider(serviceProvider);
+            return builder.Options;
+        }
     }
 }
