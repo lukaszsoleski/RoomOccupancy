c2b7b7a81d4ac0cd8b6bbae0a48e66c0bbe163fc
Łukasz Soleski <luk.soleski@gmail.com>
2019-08-28 23:09:32 +0200

    NgModel binding for the control of the start and end time of the reservation. Sets the default value dynamically.

--- RoomOccupancy.API/AngularClient/src/app/room/reservation/reservation.component.ts
+++ RoomOccupancy.API/AngularClient/src/app/room/reservation/reservation.component.ts
@@ -1,5 +1,6 @@
+import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationComponent implements OnInit {
 
-  constructor() { }
+  private now = new Date();
+  protected selectedEndTime: string;
+  protected selectedStartTime : string;
+  constructor(private toastr: ToastrService) {
+  }
 
   ngOnInit() {
+    this.selectedEndTime = moment(this.now).add(3, 'hour').format('HH:mm');
+    this.selectedStartTime = moment(this.now).add(1, 'hour').format('HH:mm');
   }
 
 }
