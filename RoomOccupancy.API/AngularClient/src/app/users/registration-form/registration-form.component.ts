b4fcaf7b1684773140365f133d72b5304b17c424
Łukasz Soleski <luk.soleski@gmail.com>
2019-09-03 17:46:16 +0200

    Registration form

--- RoomOccupancy.API/AngularClient/src/app/users/registration-form/registration-form.component.ts
+++ RoomOccupancy.API/AngularClient/src/app/users/registration-form/registration-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  public get email(): AbstractControl {
+    return this.registrationForm.get('email');
+  }
+  public get firstName(): AbstractControl {
+    return this.registrationForm.get('firstName');
+  }
+  public get lastName(): AbstractControl {
+    return this.registrationForm.get('lastName');
+  }
+  public get password(): AbstractControl {
+    return this.registrationForm.get('password');
+  }
+  public get confirmPassword(): AbstractControl {
+    return this.registrationForm.get('confirmPassword');
+  }
+
+  registrationForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  initRegistrationForm() {
+    this.registrationForm =  this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['',[Validators.required, Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$)')]],
+      ConfirmPassword: ['']
+    });
+  }
 
   ngOnInit() {
+    this.initRegistrationForm()
   }
 
 }
