a881637a734c6911c7f10acbb8dd674de61be8ad
Łukasz Soleski <luk.soleski@gmail.com>
2019-09-08 01:48:48 +0200

    Add styles to the 'create' reservation button.

--- RoomOccupancy.API/Controllers/Schedule/ReservationController.cs
+++ RoomOccupancy.API/Controllers/Schedule/ReservationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RoomOccupancy.Application.Reservations;
 using RoomOccupancy.Application.Reservations.Commands.CreateReservation;
 using System;
@@ -11,6 +12,7 @@ namespace RoomOccupancy.API.Controllers.Schedule
     public class ReservationController : BaseController
     {
         [HttpPost]
+        [Authorize(Policy = "ApiUser")]
         public async Task<IActionResult> Post(ReservationModel reservation)
         {
             return Created("api/reservation", await Mediator.Send(new CreateReservationCommand() { Reservation = reservation }));
