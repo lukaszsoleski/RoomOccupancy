ccc6dfe4ce21e14d48ae1d719445de3b548b9bfb
Łukasz Soleski <luk.soleski@gmail.com>
2019-09-12 09:47:19 +0200

    Replace content in the general email template.

---
+++ RoomOccupancy.Application/Notifications/SendEmailVerificationMessage.cs
@@ -0,0 +1,27 @@
+﻿using RoomOccupancy.Application.Infrastructure.Email;
+using RoomOccupancy.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomOccupancy.Application.Notifications
+{
+    public class EmailSender
+    {
+        private readonly IEmailTemplateSender _templateSender;
+
+        public EmailSender(IEmailTemplateSender templateSender)
+        {
+            _templateSender = templateSender;
+        }
+        public Task<SendEmailResponse> SendEmailVerificationMessage()
+        {
+
+            var message = new Message();
+
+
+            return null; // _templateSender.SendGeneralEmailAsync(messsage);
+        }
+    }
+}
--- RoomOccupancy.Infrastructure/Notifications/Templates/EmailTemplateSender.cs
+++ RoomOccupancy.Infrastructure/Notifications/Templates/EmailTemplateSender.cs
@@ -1,8 +1,10 @@
-﻿using RoomOccupancy.Application.Infrastructure.Email;
+﻿using NPOI.HSSF.UserModel;
+using RoomOccupancy.Application.Infrastructure.Email;
 using RoomOccupancy.Application.Interfaces;
 using RoomOccupancy.Application.Notifications;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Text;
@@ -12,6 +14,12 @@ namespace RoomOccupancy.Infrastructure.Notifications.Templates
 {
     public class EmailTemplateSender : IEmailTemplateSender
     {
+        private readonly INotificationService _emailService;
+
+        public EmailTemplateSender(INotificationService emailService)
+        {
+            _emailService = emailService;
+        }
         public async Task<SendEmailResponse> SendGeneralEmailAsync(Message message, string title, string content1, string content2, string buttonText, string buttonUrl)
         {
             var templateText = default(string);
@@ -19,9 +27,25 @@ namespace RoomOccupancy.Infrastructure.Notifications.Templates
             using (var reader = new StreamReader(Assembly.GetEntryAssembly().GetManifestResourceStream("RoomOccupancy.Infrastructure.Notifications.Templates.GeneralTemplate.html"), Encoding.Unicode))
             {
                 templateText = await reader.ReadToEndAsync();
+
             }
 
-            return null;
+            templateText = templateText.Replace("--Title--", title)
+                   .Replace("--Content1--", content1)
+                   .Replace("--Content2--", content2)
+                   .Replace("--ButtonText--", buttonText)
+                   .Replace("--ButtonUrl--", buttonUrl);
+            message.Content = templateText;
+            message.IsHTML = true;
+
+            var response = await _emailService.SendAsync(message);
+            if (!response.Successful && Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
+
+            return response;
+
         }
     }
 
